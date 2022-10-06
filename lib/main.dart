d510994b278c1f3899772b43ddd79a1fe915040c
ahmadobe99 <ahmadobeidat9960@gmail.com>
2022-10-06 13:32:58 +0300

    ahmad

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_8/comp/custom.dart';
 
 void main() {
-  runApp(MaterialApp(
+   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
