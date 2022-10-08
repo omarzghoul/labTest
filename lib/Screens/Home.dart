3600f7532630a37f3ff9316b6567647e31d327f6
MalekIrsheid <malekirsheid2000@gmail.com>
2022-10-08 11:06:28 +0300

    malek

---
+++ lib/Screens/Home.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/Screens/Quiz.dart';
+import 'package:myapp/comp/url.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Learn English",
+          style: TextStyle(
+              fontFamily: "Peralta", fontSize: 28, color: Colors.black),
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.deepPurple.shade100,
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(children: [
+            CircleAvatar(
+              backgroundImage: AssetImage("images/welcomepicture.jpg"),
+              radius: 170,
+            ),
+            Text(
+              "You can see link below if you want to improve your english:",
+              style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 17,
+                  fontFamily: "Peralta"),
+            ),
+            UrlFunction()
+          ]),
+        ),
+      ),
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/giphy.gif"), fit: BoxFit.fill)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple.shade400),
+                  onPressed: () {
+                    // Navigator.push(context, MaterialPageRoute(
+                    //   builder: (context) {
+                    //     return;
+                    //   },
+                    // ));
+                  },
+                  icon: Icon(
+                    Icons.image,
+                    color: Colors.black,
+                  ),
+                  label: Text(
+                    "Image Practice",
+                    style:
+                        TextStyle(color: Colors.black, fontFamily: "Peralta"),
+                  )),
+              ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple.shade400),
+                  onPressed: () {
+                    // Navigator.push(context, MaterialPageRoute(
+                    //   builder: (context) {
+                    //     return;
+                    //   },
+                    // ));
+                  },
+                  icon: Icon(
+                    Icons.audio_file,
+                    color: Colors.black,
+                  ),
+                  label: Text(
+                    "listening Practice",
+                    style:
+                        TextStyle(color: Colors.black, fontFamily: "Peralta"),
+                  )),
+              ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple.shade400),
+                  onPressed: () {
+                    // Navigator.push(context, MaterialPageRoute(
+                    //   builder: (context) {
+                    //     return;
+                    //   },
+                    // ));
+                  },
+                  icon: Icon(
+                    Icons.abc,
+                    color: Colors.black,
+                  ),
+                  label: Text(
+                    "Vocabulary Practice",
+                    style:
+                        TextStyle(color: Colors.black, fontFamily: "Peralta"),
+                  ))
+            ],
+          )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_8/comp/custom.dart';
+import 'package:myapp/Screens/Quiz.dart';
+import 'package:myapp/Screens/Home.dart';
 
 void main() {
-   runApp(MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final PageController controller = PageController();
-  int currentPage = 0;
-  List<String> questions = [
-    'Which is the fastest animal?',
-    'Which is the slowest animal?',
-    'Which is the longest animal?',
-  ];
-
-  List<String> image = [
-    'https://upload.wikimedia.org/wikipedia/commons/8/86/Strawberry_%28transparent_background%29.png',
-    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/8/86/Strawberry_%28transparent_background%29.png',
-    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-  ];
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: PageView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  controller: controller,
-                  scrollDirection: Axis.vertical,
-                  itemCount: questions.length + 1,
-                  itemBuilder: (context, index) {
-                    if (index == questions.length) {
-                      return Container(
-                        child: Text(""),
-                        color: Colors.amber,
-                      );
-                    }
-                    return custom(
-                        URL1: "${image[currentPage]}",
-                        URL2: "${image[currentPage]}",
-                        URL3: "${image[currentPage]}",
-                        URL4: "${image[currentPage]}");
-                  },
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  if (currentPage == questions.length) return;
-
-                  controller.jumpToPage(++currentPage);
-                },
-                child: const Text('Next'),
-              )
-            ],
-          ),
-        ));
+    return home();
   }
 }
-//malek 
