cb0dbde3076858b93a6e05fe7af93202309aed04
M-Taha95echo <msoudi2013@gmail.com>
2022-10-08 13:05:56 +0300

    mohammed

--- lib/comp/custom.dart
+++ lib/comp/custom.dart
@@ -4,66 +4,155 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class custom extends StatelessWidget {
-  custom({
-    required this.URL1,
-    required this.URL2,
-    required this.URL3,
-    required this.URL4,
-  });
-  Color def = Colors.white;
+  custom(
+      {required this.URL1,
+      required this.URL2,
+      required this.URL3,
+      required this.URL4,
+      required this.question});
+  Color def = Colors.grey.shade200;
 
   String? URL1;
   String? URL2;
   String? URL3;
   String? URL4;
-
+  String? question;
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            "data",
-            style: TextStyle(fontSize: 30),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              alignment: Alignment.center,
+              child: Text(
+                "$question",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: "Peralta",
+                    color: Colors.yellowAccent),
+              ),
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple.shade300,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  boxShadow: [
+                    BoxShadow(
+                        spreadRadius: 1, blurRadius: 15, offset: Offset(4, 4))
+                  ]),
+            ),
+          ),
+          SizedBox(
+            height: 30,
           ),
           Expanded(
             child: GridView.count(crossAxisCount: 2, children: [
               GestureDetector(
-                  onTap: () {},
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: Icon(Icons.close, color: Colors.red),
+                          title: Text(
+                            "Sorry , it is wrong answer ... try again ",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: "Lobster",
+                                color: Colors.redAccent),
+                          ),
+                        );
+                      },
+                    );
+                  },
                   child: Container(
-                    child: Image.network(
-                      "$URL1",
+                    child: Image.asset(
+                      "${URL1}",
                       width: 150,
                       height: 150,
                     ),
                     color: def,
                   )),
               GestureDetector(
-                  onTap: () {},
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: Icon(Icons.close, color: Colors.red),
+                          title: Text(
+                            "Sorry , it is wrong answer ... try again ",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: "Lobster",
+                                color: Colors.redAccent),
+                          ),
+                        );
+                      },
+                    );
+                  },
                   child: Container(
-                    child: Image.network(
-                      "$URL2",
+                    child: Image.asset(
+                      "${URL2}",
                       width: 150,
                       height: 150,
                     ),
                     color: def,
                   )),
               GestureDetector(
-                  onTap: () {},
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: Icon(
+                            Icons.close,
+                            color: Colors.red,
+                          ),
+                          title: Text(
+                              "Sorry , it is wrong answer ... try again  ",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: "Lobster",
+                                  color: Colors.redAccent)),
+                        );
+                      },
+                    );
+                  },
                   child: Container(
-                    child: Image.network(
-                      "$URL3",
+                    child: Image.asset(
+                      "${URL3}",
                       width: 150,
                       height: 150,
                     ),
                     color: def,
                   )),
               GestureDetector(
-                  onTap: () {},
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          icon: Icon(
+                            Icons.done,
+                            color: Colors.green,
+                          ),
+                          title: Text(
+                              "Awesome ... good for you , keep practicing,",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: "Lobster",
+                                  color: Colors.green)),
+                        );
+                      },
+                    );
+                  },
                   child: Container(
-                    child: Image.network(
-                      "$URL4",
+                    child: Image.asset(
+                      "${URL4}",
                       width: 150,
                       height: 150,
                     ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_8/comp/custom.dart';
 
 void main() {
-   runApp(MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
@@ -19,57 +19,166 @@ class _MyAppState extends State<MyApp> {
   final PageController controller = PageController();
   int currentPage = 0;
   List<String> questions = [
-    'Which is the fastest animal?',
-    'Which is the slowest animal?',
-    'Which is the longest animal?',
+    'Which of these shapes belongs to the boy ?',
+    'Which of these Picturs is Orange fruit ?',
+    'Which of these picturs is Circul ?',
+    'Which of these picturs is Phone ?',
+    'Which of these shapes belongs to Sun?',
+    'Which of these Picturs is Teacher ?',
+    'Which of these picturs is Cat ?',
+    'Which of these picturs is Car?',
+    'What is of these palces is Mosque ?',
+    "what is the correct time .. Ten o'clock ?"
   ];
 
   List<String> image = [
-    'https://upload.wikimedia.org/wikipedia/commons/8/86/Strawberry_%28transparent_background%29.png',
-    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/8/86/Strawberry_%28transparent_background%29.png',
-    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    'images/1.png',
+    'images/5.png',
+    'images/9.png',
+    'images/13.png',
+    'images/17.webp',
+    'images/21.webp',
+    'images/25.webp',
+    'images/29.webp',
+    'images/33.webp',
+    'images/37.png'
+  ];
+  List<String> image1 = [
+    'images/2.png',
+    'images/6.png',
+    'images/10.jpg',
+    'images/14.png',
+    'images/18.webp',
+    'images/22.png',
+    'images/26.webp',
+    'images/30.webp',
+    'images/34.webp',
+    'images/38.png'
+  ];
+  List<String> image2 = [
+    'images/3.png',
+    'images/7.webp',
+    'images/11.png',
+    'images/15.png',
+    'images/19.webp',
+    'images/23.webp',
+    'images/27.webp',
+    'images/31.webp',
+    'images/35.png',
+    'images/39.png'
+  ];
+  List<String> image3 = [
+    'images/4.png',
+    'images/8.webp',
+    'images/12.png',
+    'images/16.png',
+    'images/20.webp',
+    'images/24.png',
+    'images/28.webp',
+    'images/32.png',
+    'images/36.webp',
+    'images/40.jpg'
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple.shade100,
+          title: Text(
+            "English for begginers",
+            style: TextStyle(color: Color.fromARGB(255, 13, 81, 136)),
+          ),
+          leading: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.home,
+                color: Color.fromARGB(255, 13, 81, 136),
+              )),
+        ),
         body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: PageView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  controller: controller,
-                  scrollDirection: Axis.vertical,
-                  itemCount: questions.length + 1,
-                  itemBuilder: (context, index) {
-                    if (index == questions.length) {
-                      return Container(
-                        child: Text(""),
-                        color: Colors.amber,
-                      );
-                    }
-                    return custom(
+          child: Container(
+            color: Colors.grey.shade200,
+            child: Column(
+              children: [
+                Expanded(
+                  child: PageView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    controller: controller,
+                    scrollDirection: Axis.vertical,
+                    itemCount: questions.length + 1,
+                    itemBuilder: (context, index) {
+                      if (index == questions.length) {
+                        return Container(
+                          color: Colors.amber.shade300,
+                          child: Column(children: [
+                            Container(
+                              alignment: Alignment.topCenter,
+                              child: ElevatedButton.icon(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.info),
+                                  label: Text("information")),
+                            ),
+                            Text(
+                              "- Boy: it is a noun : A boy is a child who will grow up to be a man.",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                                "Synonyms: lad, kid [informal], youth, fellow [old-fashioned] ",
+                                style: TextStyle(fontSize: 20)),
+                            Text(
+                                "-Fruit or a fruit is something which grows on a tree or bush and which contains seeds or a stone covered by a substance that you can eat.",
+                                style: TextStyle(fontSize: 20)),
+                            Text(
+                                "- shape of an object, a person, or an area is the appearance of their outside edges or surfaces, for example whether they are round, square, curved, or fat.",
+                                style: TextStyle(fontSize: 20)),
+                            Text(
+                                "- An animal is a living creature such as a dog, lion, or rabbit, rather than a bird, fish, insect, or human being.",
+                                style: TextStyle(fontSize: 20)),
+                            Text(
+                                "- A planet is a large, round object in space that moves around a star. The Earth is a planet.",
+                                style: TextStyle(fontSize: 20)),
+                            Text(
+                                "- A building is a structure that has a roof and walls, for example a house or a School.",
+                                style: TextStyle(fontSize: 20)),
+                            Text(
+                                "- Transport refers to any vehicle that you can travel in or carry goods in.",
+                                style: TextStyle(fontSize: 20)),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              "Thank you to use This App",
+                              style: TextStyle(
+                                  fontSize: 20, fontFamily: "Peralta"),
+                            )
+                          ]),
+                        );
+                      }
+                      return custom(
                         URL1: "${image[currentPage]}",
-                        URL2: "${image[currentPage]}",
-                        URL3: "${image[currentPage]}",
-                        URL4: "${image[currentPage]}");
-                  },
+                        URL2: "${image1[currentPage]}",
+                        URL3: "${image2[currentPage]}",
+                        URL4: "${image3[currentPage]}",
+                        question: "${questions[currentPage]}",
+                      );
+                    },
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  if (currentPage == questions.length) return;
+                TextButton(
+                  onPressed: () {
+                    if (currentPage == questions.length) return;
 
-                  controller.jumpToPage(++currentPage);
-                },
-                child: const Text('Next'),
-              )
-            ],
+                    controller.jumpToPage(++currentPage);
+                  },
+                  child: Text(
+                    'Next',
+                    style: TextStyle(fontSize: 20, fontFamily: "Peralta"),
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
 }
-//malek 
